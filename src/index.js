c8b8367277739b95d32e8a02300adb1865ce0b77
jcleow <jc.leow94@gmail.com>
2021-01-13 11:27:57 +0800

    First commit - completed till comfortable

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 import './styles.scss';
+import React from 'react';
+import { render } from 'react-dom';
+import data from './App.jsx';
+import moment from 'moment';
 
-console.log('hello');
+const rootElement = document.createElement('div');
+const itemsList = data.items.map((item,index) => 
+<li key={index.toString()}>
+  <p>id:{item.id}</p>
+  <p>name:{item.name}</p>
+  <p>description:{item.description}</p>
+  <p>created:{moment(item.createdAt).fromNow()}</p>
+  <p>updated:{moment(item.updatedAt).fromNow()}</p>
+</li> 
+)
+const myEl = (
+<ul>
+  {itemsList}  
+</ul>
+)
+
+document.body.appendChild(rootElement);
+
+render(myEl,rootElement);
